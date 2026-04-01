153cdb3072808241c89453de9eb49bbcf5df4094
01110100chony <anthonycristian@alunos.utfpr.edu.br>
2026-04-01 16:00:36 -0300

    commentary of hamming

--- c/hamming/hamming.c
+++ c/hamming/hamming.c
@@ -22,23 +22,23 @@ The Hamming distance is only defined for sequences of equal length, so an attemp
 */
 
 int compute(const char *lhs, const char *rhs){
-    int size_l = strlen(lhs); // determining the samples length
+    int size_l = strlen(lhs); // determining the samples length for comparations terms
     int size_r = strlen(rhs);
 
-    if( ( size_l != size_r ) ){
-        return -1;
-    }
-   
-    if((size_l == size_r)==0){
-        return 0;
-    }
-    else{
-    int count = 0;
-        for(int i = 0 ; i < size_l ; i++){
-            if(lhs[i]!=rhs[i]){
-                count++;
-            }
+        if( ( size_l != size_r ) ){
+            return -1; //they need to be equal length to return valid values
         }
-        return count;
-}
+        if( (size_l == size_r) ==0 ){
+            return 0; //if they are empty, also return 0;
+        }
+        else{
+                
+            int count = 0;//counter of diff
+                    for(int i = 0 ; i < size_l ; i++){
+                        if(lhs[i]!=rhs[i]){  //if any char of the array1 is different from the char of array2,+1 to counter of diff
+                            count++;
+             }
+        }
+        return count; //returning the number of diff between the arrays
     }
+}
